b4232032cb765a2677890f7104b1e1798a994c12
Lukas <lukastrakim@gmail.com>
2025-03-28 15:37:06 +0000

    crashes on inithook and i dont know why

--- machineframe/driver.cpp
+++ machineframe/driver.cpp
@@ -25,7 +25,7 @@ BOOLEAN RestoreFrameCallback(PVOID context, BOOLEAN handled) {
     machine_frame->Rsp = nmi_core_infos[processor_index].prev_rsp;
     kprcb_threads->CurrentThread = nmi_core_infos[processor_index].prev_current_thread;
     kprcb_threads->NextThread = nmi_core_infos[processor_index].prev_next_thread;
-    kprcb_threads->IdleThread = nmi_core_infos[processor_index].prev_idle_thread;
+    *reinterpret_cast<PBOOLEAN>(reinterpret_cast<PUCHAR>(kprcb_threads->IdleThread) + 0x71) = nmi_core_infos[processor_index].prev_active;
 
     LOG_DEBUG("Swapped back thread");
     if (callback_parent) {
@@ -66,7 +66,7 @@ VOID HalPreprocessNmiHook(ULONG arg1) {
     nmi_core_infos[processor_index].prev_rsp = machine_frame->Rsp;
     nmi_core_infos[processor_index].prev_current_thread = kprcb_threads->CurrentThread;
     nmi_core_infos[processor_index].prev_next_thread = kprcb_threads->NextThread;
-    nmi_core_infos[processor_index].prev_idle_thread = kprcb_threads->IdleThread;
+    nmi_core_infos[processor_index].prev_active = *reinterpret_cast<PBOOLEAN>(reinterpret_cast<PUCHAR>(kprcb_threads->IdleThread) + 0x71);
 
     /*
         We will spoof as the current core's idle system thread
@@ -75,10 +75,10 @@ VOID HalPreprocessNmiHook(ULONG arg1) {
     */
     
     machine_frame->Rip = PoIdle;
-    machine_frame->Rsp = reinterpret_cast<ULONGLONG>(idle_thread_list[processor_index].initial_stack) - 0x70;
-    kprcb_threads->CurrentThread = idle_thread_list[processor_index].thread;
+    machine_frame->Rsp = *reinterpret_cast<PULONGLONG>(reinterpret_cast<PUCHAR>(kprcb_threads->IdleThread) + 0x28) - 0x70;
+    kprcb_threads->CurrentThread = kprcb_threads->IdleThread;
     kprcb_threads->NextThread = nullptr;
-    kprcb_threads->IdleThread = idle_thread_list[processor_index].thread;
+    *reinterpret_cast<PBOOLEAN>(reinterpret_cast<PUCHAR>(kprcb_threads->IdleThread) + 0x71) = TRUE;
 }
 
 NTSTATUS InitHook() {
@@ -115,7 +115,6 @@ VOID Unload(PDRIVER_OBJECT driver_object) {
     
     LOG_DEBUG("Unloading");
     ExFreePoolWithTag(nmi_core_infos, NMI_CORE_INFO_TAG);
-    ExFreePoolWithTag(idle_thread_list, NMI_THREAD_LIST_TAG);
     Unhook();
 
     UNICODE_STRING symbolic_link = RTL_CONSTANT_STRING(L"\\??\\frame");
@@ -155,34 +154,6 @@ NTSTATUS SigscanPoIdle() {
     return PoIdle ? STATUS_SUCCESS : STATUS_UNSUCCESSFUL;
 }
 
-NTSTATUS GetIdleThreads() {
-    if (!PsIdleProcess) {
-        LOG_ERROR("PsIdleProcess not found");
-        return STATUS_UNSUCCESSFUL;
-    }
-    // just using offsets because EPROCESS and KTHREAD contain a lot of structs I would need to specify
-    // offset to ThreadListLock
-    PEX_PUSH_LOCK thread_lock = PEX_PUSH_LOCK(reinterpret_cast<ULONG_PTR>(PsIdleProcess) + 0x860);
-    ExAcquirePushLockExclusive(thread_lock);
-    // offset to ThreadListHead
-    PLIST_ENTRY list_head = reinterpret_cast<PLIST_ENTRY>(PsIdleProcess) + 0x5e0;
-
-    PLIST_ENTRY list_entry = list_head;
-    for (ULONG i = 0;
-        i < KeQueryActiveProcessorCount(0) && list_entry && list_entry->Flink != list_head;
-        list_entry = list_entry->Flink
-        ) {
-        // offset from ThreadListEntry back to start of KTHREAD
-        PKTHREAD thread = reinterpret_cast<PKTHREAD>(list_entry - 0x2f8);
-        idle_thread_list[i].thread = thread;
-        // offset to InitialStack
-        idle_thread_list[i].initial_stack = reinterpret_cast<PVOID>(reinterpret_cast<ULONG_PTR>(thread) + 0x28);
-        LOG_DEBUG("Stored thread %d", i);
-    }
-
-    return STATUS_SUCCESS;
-}
-
 extern "C"
 NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
     UNREFERENCED_PARAMETER(registry_path);
@@ -191,7 +162,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path
 
     auto status = STATUS_SUCCESS;
     do {
-
         nmi_list_head = SigscanKiNmiCallbackListHead();
         status = SigscanPoIdle();
         if (!NT_SUCCESS(status)) {
@@ -207,21 +177,6 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path
         if (!nmi_core_infos) {
             LOG_ERROR("Failed to allocate nmi core info array");
             status = STATUS_INSUFFICIENT_RESOURCES;
-            break;
-        }
-        
-        idle_thread_list = static_cast<PTHREAD_INFO>(ExAllocatePool2(POOL_FLAG_NON_PAGED,
-            num_processors * sizeof(THREAD_INFO),
-            NMI_THREAD_LIST_TAG));
-        if (!idle_thread_list) {
-            LOG_ERROR("Failed to allocate idle thread array");
-            status = STATUS_INSUFFICIENT_RESOURCES;
-            break;
-        }
-
-        status = GetIdleThreads();
-
-        if (!NT_SUCCESS(status)) {
             break;
         }
 
--- machineframe/driver.hpp
+++ machineframe/driver.hpp
@@ -1,6 +1,4 @@
-constexpr ULONG IA32_GS_BASE = 0xC0000101;
-constexpr ULONG NMI_CORE_INFO_TAG = '0imn';
-constexpr ULONG NMI_THREAD_LIST_TAG = '1imn';
+constexpr ULONG NMI_CORE_INFO_TAG = 'imn';
 
 typedef VOID(*PHAL_PREPROCESS_NMI)(ULONG arg1);
 PHAL_PREPROCESS_NMI HalPreprocessNmiOriginal = nullptr;
@@ -11,13 +9,8 @@ typedef struct _NMI_CORE_INFO {
     ULONG64 prev_rsp;
     PKTHREAD prev_current_thread;
     PKTHREAD prev_next_thread;
-    PKTHREAD prev_idle_thread;
+    BOOLEAN prev_active;
 } NMI_CORE_INFO, * PNMI_CORE_INFO;
-
-typedef struct _THREAD_INFO {
-    PKTHREAD thread;
-    PVOID initial_stack;
-} THREAD_INFO, *PTHREAD_INFO;
 
 typedef struct _KPRCB_THREADS {
     PKTHREAD CurrentThread;
@@ -31,4 +24,3 @@ PKNMI_HANDLER_CALLBACK callback_parent = nullptr;
 KNMI_HANDLER_CALLBACK restore_callback{ nullptr, reinterpret_cast<void(*)()>(RestoreFrameCallback), nullptr, nullptr };
 
 ULONGLONG PoIdle;
-PTHREAD_INFO idle_thread_list = nullptr;
